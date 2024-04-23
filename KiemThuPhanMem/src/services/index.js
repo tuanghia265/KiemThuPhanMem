221e212402443c2f98d57ca2494315e3be833ee8
Lê Tuấn Nghĩa <92085071+tuanghia265@users.noreply.github.com>
2024-04-23 09:10:01 +0700

    Delete KiemThuPhanMem directory

--- KiemThuPhanMem/src/services/index.js
+++
@@ -1,3 +0,0 @@
-export { default as adminService } from './adminService';
-
-
--- KiemThuPhanMem/src/services/userService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-const handleLogin=(email,pwd)=>{
-    return axios.post('/api/login',{email,pwd});
-}
-export {handleLogin};
